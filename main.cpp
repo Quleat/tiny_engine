2e160ea2d74b2145a726e4f55b38fe769e7c4525
Quleat So <quleat@localhost.localdomain>
2026-03-18 08:54:11 +0300

    Trying to create a window (Failure)

--- main.cpp
+++ main.cpp
@@ -2,6 +2,38 @@
 #include <GLFW/glfw3.h>
 #include <iostream>
 
+void framebuffer_size_callback(GLFWwindow* , int width, int height);
+
 int main(int argc, char *argv[]){
+	//---------------CREATING A WINDOW-----------------------
+	glfwInit();
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+	GLFWwindow* window = glfwCreateWindow(800, 600, "LearnOpenGL", NULL, NULL);
+	if(window == NULL){
+		std::cerr << "Failed to crate window!\n";
+		glfwTerminate();
+		return -1;
+	}
+	glfwMakeContextCurrent(window);
 	std::cout << "Hello world!\n";
+
+	////------------GLAD------------------------------------
+	if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)){
+		std::cerr << "Failed to initialize GLAD!\n";
+		return -1;
+	}
+
+	glViewport(0,0,800,600);
+	glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+
+	while(!glfwWindowShouldClose(window)){
+		glfwSwapBuffers(window);
+		glfwPollEvents();
+	}
+}
+
+void framebuffer_size_callback(GLFWwindow* , int width, int height){
+	glViewport(0,0,width,height);
 }
