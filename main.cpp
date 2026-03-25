eaebdeafb683b7af0306e065e4d1b333798e99e6
Quleat <kintoxo86@gmail.com>
2026-03-25 18:00:49 +0300

    double trouble

--- main.cpp
+++ main.cpp
@@ -105,32 +105,61 @@ int main(int, char *[]){
 	//-----------DRAWING----------------------------------
 	
 	float verticies[] = {
-		-0.5f, -0.5f, 0.0f,
-		0.5f, -0.5f, -0.0f,
-		0.0f, 0.5f, 0.0f
+		-0.75f, -0.5f, 0.0f,
+    -0.25f, -0.5f, 0.0f,
+    -0.5f, 0.25f, 0.0f,
+
+    0.25f, -0.5f, 0.0f,
+    0.75f, -0.5f, 0.0f,
+    0.5f, 0.25f, 0.0f
 	};
-	unsigned int VBO, VAO;
-	glGenVertexArrays(1, &VAO);
-	glBindVertexArray(VAO);
-	glGenBuffers(1, &VBO);
-	glBindBuffer(GL_ARRAY_BUFFER, VBO);
-	glBufferData(GL_ARRAY_BUFFER, sizeof(verticies), verticies, GL_STATIC_DRAW);
+  /*unsigned int indicies[] = {
+    0, 1, 3,
+    1, 2, 3
+  };*/
+
+  //VAO1
+	unsigned int VBO[2], VAO[2];
+	glGenVertexArrays(2, VAO);
+	glBindVertexArray(VAO[0]);
+
+	glGenBuffers(2, VBO);
+
+	glBindBuffer(GL_ARRAY_BUFFER, VBO[0]);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(verticies) / 2, verticies, GL_STATIC_DRAW);
 
 	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void *)0);
 	glEnableVertexAttribArray(0);
+  glBindVertexArray(0);
 
-	
-		
+  //VAO2
+  glBindVertexArray(VAO[1]);
+  glBindBuffer(GL_ARRAY_BUFFER, VBO[1]);
+  glBufferData(GL_ARRAY_BUFFER, sizeof(verticies) / 2, &verticies[9], GL_STATIC_DRAW); //why not verticies + (9 * sizeof(float))
+  glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void *)0);
+  glEnableVertexAttribArray(0);
+  glBindVertexArray(0);
+
+  /*glGenBuffers(1, &EBO);
+  glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+  glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indicies), indicies, GL_STATIC_DRAW);*/
 
 	while(!glfwWindowShouldClose(window)){
 		processInput(window);
 
+    glPolygonMode(GL_FRONT_AND_BACK, /*GL_FILL*/GL_LINE);
+
 		glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
 		glClear(GL_COLOR_BUFFER_BIT);
 
-	glUseProgram(shaderProgram);
-	glBindVertexArray(VAO);
-	glDrawArrays(GL_TRIANGLES, 0, 3);
+	  glUseProgram(shaderProgram);
+	  glBindVertexArray(VAO[0]);
+    //glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+	  //glDrawArrays(GL_TRIANGLES, 0, 3);
+    glDrawArrays(GL_TRIANGLES, 0, 3);
+
+    glBindVertexArray(VAO[1]);
+    glDrawArrays(GL_TRIANGLES, 0, 3);
 
 		glfwSwapBuffers(window);
 		glfwPollEvents();
